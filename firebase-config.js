562f011efb2387b1f61599e75e60a80c61151f58
shreya <shreya@gmail.com>
2025-08-01 08:32:15 +0530

    Initial commit - Intern Portal

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCBEnL4US6_qvZY9pT2_UNI6qh4IH0E6Fk",
+  authDomain: "portal-2771e.firebaseapp.com",
+  projectId: "portal-2771e",
+  storageBucket: "portal-2771e.firebasestorage.app",
+  messagingSenderId: "677731062923",
+  appId: "1:677731062923:web:c83375cad0e3ae380ff08e",
+  measurementId: "G-6QTGS8CTLJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
